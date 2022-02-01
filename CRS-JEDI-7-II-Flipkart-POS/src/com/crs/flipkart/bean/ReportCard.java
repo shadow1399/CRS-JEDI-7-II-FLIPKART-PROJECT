62733e2d7f3bc67d3ca5690d41340aa54b1c099a
shubham sharma <shubhamsharma000231@gmail.com>
2022-02-01 17:56:30 +0530

    Student CRS Menu class and some bean class updated.

--- CRS-JEDI-7-II-Flipkart-POS/src/com/crs/flipkart/bean/ReportCard.java
+++ CRS-JEDI-7-II-Flipkart-POS/src/com/crs/flipkart/bean/ReportCard.java
@@ -1,36 +1,75 @@
 package com.crs.flipkart.bean;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ReportCard {
 	private int Semester;
 	private int rollNo;
 	private float gpa;
-	private Map<String,Float> grades;
+	private HashMap<String,Float> grades = new HashMap<String,Float>();
+	
+	
+	/**
+	 * @param semester
+	 * @param rollNo
+	 * @param gpa
+	 * @param grades
+	 */
+	public ReportCard(int semester, int rollNo, float gpa, HashMap<String, Float> grades) {
+		super();
+		Semester = semester;
+		this.rollNo = rollNo;
+		this.gpa = gpa;
+		this.grades = grades;
+	}
+	/**
+	 * @return the semester
+	 */
 	public int getSemester() {
 		return Semester;
 	}
+	/**
+	 * @param semester the semester to set
+	 */
 	public void setSemester(int semester) {
 		Semester = semester;
 	}
+	/**
+	 * @return the rollNo
+	 */
 	public int getRollNo() {
 		return rollNo;
 	}
+	/**
+	 * @param rollNo the rollNo to set
+	 */
 	public void setRollNo(int rollNo) {
 		this.rollNo = rollNo;
 	}
+	/**
+	 * @return the gpa
+	 */
 	public float getGpa() {
 		return gpa;
 	}
+	/**
+	 * @param gpa the gpa to set
+	 */
 	public void setGpa(float gpa) {
 		this.gpa = gpa;
 	}
-	public Map<String, Float> getGrades() {
+	/**
+	 * @return the grades
+	 */
+	public HashMap<String, Float> getGrades() {
 		return grades;
 	}
-	public void setGrades(Map<String, Float> grades) {
+	/**
+	 * @param grades the grades to set
+	 */
+	public void setGrades(HashMap<String, Float> grades) {
 		this.grades = grades;
 	}
-	
 	
 }
